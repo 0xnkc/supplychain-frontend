cb058f66fe55fa242587d43986e1dde6bbcd9d2f
Nikhil K C <nikhilkc96@gmail.com>
2019-03-10 20:37:11 +0530

    Update track.php
    added guage

--- track.php
+++ track.php
@@ -15,12 +15,14 @@
   <body>
     <h3>Tracking ID:XXXXXXXX</h3>
     <br>
-    <h5>Food storage temperature:XX</h5>
-   <h5>Food storage humidity:XX</h5>
+    <div id="chart_div" style="width: 400px; height: 120px;"></div>
     <!--The div element for the map -->
     <div id="map"></div>
 
-
+<div class="outer" style="margin: 0 auto; width: 100px;">
+    <div id="container-speed" class="chart-container"></div>
+   
+</div>
     <script>
 // Initialize and add the map
 function initMap() {
@@ -39,8 +41,50 @@ function initMap() {
   var marker = new google.maps.Marker({position: uluru, map: map,icon: icon ,zoom: 10 });
 }
     </script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
+
+<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?sensor=true&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlI4CzgEkZkgobD13qJ8DngUlRBXB2Qsk&callback=initMap">
+    </script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+   <script type="text/javascript">
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Temperature', 50],
+          ['Humidity', 40],
+        ]);
+
+        var options = {
+          width: 400, height: 120,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 13000);
+        setInterval(function() {
+          data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 5000);
+        setInterval(function() {
+          data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+          chart.draw(data, options);
+        }, 26000);
+      }
     </script>
   </body>
 </html>
